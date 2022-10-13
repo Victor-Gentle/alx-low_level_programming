9b81bd6333a0b50a5ac69087a0b074a6777af793
Victor-Gentle <gentlevictor2000@gmail.com>
2022-10-13 11:10:38 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- *  main - Main Function of my project  
- *  Return: Always 0 (Success)
+ * main - Main Function of my project  
+ * Return: Always 0 (Success)
  */
 
 int main(void)
