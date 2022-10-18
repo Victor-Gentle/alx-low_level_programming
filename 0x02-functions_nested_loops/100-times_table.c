fcc3702ec398ffdb52e12ea723c4996ba8db7849
Victor-Gentle <gentlevictor2000@gmail.com>
2022-10-18 15:53:52 -0700

    N

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -25,10 +25,6 @@ void print_times_table(int n)
 	{
 		_putchar((result / 10) + '0');
 	}
-	else if (result >= 100)
-	{
-		_putchar((result / 100) + '0');
-	}
 	else
 	{
 		_putchar(' ');
@@ -38,9 +34,12 @@ void print_times_table(int n)
 	{
 		_putchar(',');
 		_putchar(' ');
+	
+	}
+	}
+	_putchar((result / 10) + '0');
+	
 	}
 	_putchar('\n');
-	}
-	}
 	}
 }
