630800fe69aab772535dc6cbc60e0476c34c96ac
Victor-Gentle <gentlevictor2000@gmail.com>
2022-10-18 01:45:09 -0700

    1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - program that prints _putchar followed by \n
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char school[] = "_putchar";
+
+	int c;
+	for (c = 0, c < 8, c++)
+	{
+		_putchar(school[c]);
+	}
+	_putchar('\n');
+	return (0);
+}
+
+
