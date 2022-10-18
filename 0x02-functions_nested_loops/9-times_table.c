07176daf037eadf0c72c5e5ad4424678134cd55a
Victor Gentle <113617445+Victor-Gentle@users.noreply.github.com>
2022-10-18 23:14:31 +0100

    C - Function Nested Loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,20 +12,26 @@ void times_table(void)
 
 	for (digit = 0; digit <= 9; digit++)
 	{
-		_putchar(0);
-	for (mult = 0; mult <= 9 ; mult++)
-	{
+		_putchar('0');
 		_putchar(',');
 		_putchar(' ');
-		result = digit * mult;
-	if (result <= 9)
+		
+	for (mult = 1; mult <= 9 ; mult++)
 	{
-		_putchar(' ');
+		result = digit * mult;
+	if ((result / 10) > 0)
+	{
+		_putchar((result / 10) + '0');
 	}
 	else
 	{
-		_putchar((result / 10) + '0');
-		_putchar((result % 10) + '0');
+		_putchar(' ');
+	}
+	_putchar((result % 10) + '0');
+	if (mult < 9)
+	{
+		_putchar(',');
+		_putchar(' ');
 	}
 	}
 	_putchar('\n');
