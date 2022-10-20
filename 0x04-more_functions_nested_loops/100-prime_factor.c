1a0a52494cd307bad8c9adfbdf856266f66b8e3f
Victor-Gentle <gentlevictor2000@gmail.com>
2022-10-20 16:24:38 -0700

    N

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -10,25 +10,16 @@
 int main(void)
 {
 int i;
-long num = 612852475143,
+long int num = 612852475143,
 
-while (i < (num / 2))
+for (i = 2; i < num; i++)
 {
-if ((num % i) == 0)
+if (num % i == 0)
 {
-num /= 2;
-continue;
+num = num / i;
 }
-for (i = 3; i < (num / 2); i += 2)
-{
-if ((num % i) == 0)
-{
-num /= i;
-}
-}
-}
-printf("%d\n", num);
+printf("%ld\n", num);
 return (0);
+}
 _putchar('\n');
-
 }
