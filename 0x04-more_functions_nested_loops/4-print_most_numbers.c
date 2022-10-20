ac44a8d4c18eeaac6a05eaceea03b10492b35a41
Victor Gentle <113617445+Victor-Gentle@users.noreply.github.com>
2022-10-20 19:13:46 +0100

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,13 +8,13 @@
 
 void print_most_numbers(void)
 {
-int i = 0;
+int i;
 
-for (i <= '9'; i++)
+for (i = 0; i <= 9; i++)
 {
-if (i != '2' && i != '4')
+if (i != 2 && i != 4)
 {
-_putchar(i);
+_putchar((i % 10) + '0');
 }
 }
 _putchar('\n');
