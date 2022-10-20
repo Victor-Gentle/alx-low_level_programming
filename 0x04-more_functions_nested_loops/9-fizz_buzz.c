539f3b322b352f598e37f2a906f022c763d0f4d2
Victor Gentle <113617445+Victor-Gentle@users.noreply.github.com>
2022-10-20 19:52:48 +0100

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,20 +16,26 @@ for (i = 1; i <= 100; i++)
 {
 if ((i % 3) == 0)
 {
-printf("%s", "Fizz ");
+printf("Fizz");
 }
 else if ((i % 5) == 0)
 {
-printf("%s", "Buzz ");
+printf("Buzz");
 }
 else if (i % 3 == 0 && i % 5 == 0)
 {
-printf("%s", "FizzBuzz ");
+printf("FizzBuzz");
 }
 else
 {
-printf("%d ", i);
+printf("%d", i);
 }
+if (i == 100)
+{
+continue;
+}
+printf(" ");
 }
 printf("\n");
+return (0);
 }
