a74aee8b5c7485554f3c186f6d80eb2644935df7
Victor Gentle <113617445+Victor-Gentle@users.noreply.github.com>
2022-10-26 21:05:20 +0100

    More pointers and arrays

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,22 +4,19 @@
  * _strcmp - function that compares two strings
  * @s1: first string
  * @s2: second string
- * Return: diff
+ * Return: 0 if s1 and s2 are equals
  */
 
 int _strcmp(char *s1, char *s2)
 {
-	int diff = 0;
-	int i = 0;
-
-	while (s1[i])
+	int i;
+	
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 	{
 		if (s1[i] != s2[i])
 		{
-			diff = ((s1[i] - '\0') - (s2[i] - '\0'));
-			break;
+			return (s1[i] - s2[i]);
 		}
-		i++
 	}
-	return (diff);
+	return (0);
 }
