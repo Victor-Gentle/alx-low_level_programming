4330d58f7a25bb5d37a7fd8187f0c6f6f9b843e9
Victor-Gentle <gentlevictor2000@gmail.com>
2022-10-31 07:39:36 -0700

    Even more pointers

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -8,7 +8,7 @@
 
 void print_chessboard(char (*a)[8])
 {
-	unsigned init i, j;
+	unsigned int i, j;
 
 	for (i = 0; i < 8; i++)
 	{
--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 
 /**
- * print_diagsums - prints the sum of the two diagonals of a square matrix of integers
+ * print_diagsums - prints the sum of the two diagonals
+ * of a square matrix of integers
  * @a: Pointer to an array
  * @size: Size of the matrix
  * Return: void
