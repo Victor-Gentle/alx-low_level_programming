20efb4eaf8ab63b3db8796439287df2a091f55b8
Victor-Gentle <gentlevictor2000@gmail.com>
2022-10-31 07:12:34 -0700

    Even more pointers

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strstr - locates a substring
+ * @needle: Character to check
+ * @haystack: Character to look for
+ * Return: Pointer
+ */
+
+char *_strstr(char *haystack, char *needle);
+{
+	while (*haystack)
+	{
+		char *src = haystack;
+		char *sub = needle;
+
+		while (*haystack && *sub && *haystack == *sub)
+		{
+			haystack++;
+			sub++;
+		}
+		if (!*sub)
+			return (src);
+		haystack = src + 1;
+	}
+	return (0);
+}
+
+
