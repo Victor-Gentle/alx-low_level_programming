676d46c664afed5b1006256a9846d88590ce6bad
Victor Gentle <113617445+Victor-Gentle@users.noreply.github.com>
2022-11-04 21:03:59 +0100

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,4 @@
 #include <stdio.h>
-#include "main.h"
-
-void print_string(char *str);
 
 /**
  *main - prints its name, followed by a new line
@@ -12,25 +9,6 @@ void print_string(char *str);
 
 int main(int _attribute_((_unused_)) argc, char *argv[])
 {
-	print_string(argv[0]);
-	_putchar('\n');
-
+	printf("%d\n", agrc);
 	return (0);
-}
-
-/**
- * print_string - prints ll char of a string
- * @str: Pointer to string
- * Return: void
- */
-
-void print_string(char *str)
-{
-	int i = 0;
-
-	while (str[i] != '\0')
-	{
-		_putchar(str[i]);
-		i++;
-	}
 }
