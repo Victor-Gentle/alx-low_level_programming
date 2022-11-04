77f35357766a1ce1fa3d57df900eb2e9a9ce4908
Victor-Gentle <gentlevictor2000@gmail.com>
2022-11-04 07:03:36 -0700

    C argc, argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - prints its name, followed by a new line
+ *main - prints its name, followed by a new line
  *@argc: counts arguments
  *@argv: array of arguments
- * Return: 0
+ *Return: 0
  */
 
 int main(int _attribute_((_unused_)) argc, char *argv[])
