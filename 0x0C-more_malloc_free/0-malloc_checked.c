e8d6891074b9af72d46aafdb79aa2d1d023fc7ee
Victor-Gentle <gentlevictor2000@gmail.com>
2022-11-10 00:33:21 -0800

    More malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * malloc_checked - function that allocates memory using
+ * @b: size of memory to be allocated
+ * Return: pointer to the allocated memory (SUCCESS)
+ * 98 if insufficient memory was available (FAILURE)
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+	if (p == NULL)
+		exit(98);
+	return (p);
+}
---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - allocates memory for an array, using malloc
+ * @nmeb: number of elements of array
+ * @size: size of each element of array
+ * Return: pointer to be allocated memory (SUCCESS)
+ * NULL if @nmemb or @size is 0 (FAILURE) or
+ * NULL if insufficient memory was available (FAILURE)
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	char *ptr;
+
+	if (nmemb == 0 || size == 0)
+		return ('\0');
+
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+		return ('\0');
+
+	for (i = 0; i < nmemb * size; i++)
+		ptr[i] = 0;
+
+	return ((void *)ptr);
+}
