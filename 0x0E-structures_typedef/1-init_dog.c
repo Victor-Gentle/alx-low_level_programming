05965b1594cfb0335e580aa33624d3358d754f4b
Victor Gentle <113617445+Victor-Gentle@users.noreply.github.com>
2022-11-14 19:34:58 +0100

    Update 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib.h>
 
 /**
  * init_dog - initialize a variable of type struct dog
@@ -11,8 +12,8 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != NULL)
 	{
-		(*d).name = name;
+		d->name = name;
 		(*d).age = age;
-		(*d).owner = owner;
+		d->owner = owner;
 	}
 }
