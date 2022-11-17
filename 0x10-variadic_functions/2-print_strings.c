ee1616c34999dc057feb98c22431884b5971c3a1
Victor Gentle <113617445+Victor-Gentle@users.noreply.github.com>
2022-11-17 15:57:58 +0100

    Update 2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -8,8 +8,6 @@
 
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-void print_strings(const char *separator, const unsigned int n, ...)
-{
 va_list valist;
 unsigned int i;
 char *p;
